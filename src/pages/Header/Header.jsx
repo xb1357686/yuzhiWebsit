410ce7249d94df240d2d80a33b81641a3d87a461
xuwen <xuwen@michaels.com>
2021-08-07 15:01:42 +0800

    add banner and content

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -6,13 +6,15 @@ import {
   MenuButton,
   Button,
   Box,
+  MenuList,
+  MenuItem,
 } from "@chakra-ui/react";
 import Logo from "../../assets/logo.jpeg";
 
 function Header() {
   return (
-    <Box>
-      <Box w="1160px" h="74px" p="20px 0" alignItems="center" m="0 auto">
+    <Box position="fixed" top="0" h="74px" w="100%" p="20px 0" bg="#fff">
+      <Box w="1160px" alignItems="center" m="0 auto">
         <Flex>
           <Flex mr="100px">
             <Image boxSize="30px" src={Logo} alt="logo" mr="10px" />
@@ -22,6 +24,14 @@ function Header() {
           </Flex>
           <Menu>
             <MenuButton p="0 30px">产品中心</MenuButton>
+            <MenuList>
+              <MenuItem>硬件产品</MenuItem>
+              <MenuItem>软件产品</MenuItem>
+              <MenuItem>课程体系</MenuItem>
+              <MenuItem>数智化创新领袖</MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
             <MenuButton p="0 30px">科创教育解决方案</MenuButton>
             <MenuButton p="0 30px">新闻中心</MenuButton>
             <MenuButton p="0 30px">关于我们</MenuButton>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,24 @@
-import { Image, Box } from "@chakra-ui/react";
-import BannerImage from "../BannerImage/BannerImage";
+import { Image, Box, Text } from "@chakra-ui/react";
+import Banner from "../Banner/Banner";
+import Content from "../Content/Content";
+import banner1 from "../../assets/banner1.jpg";
+import banner2 from "../../assets/banner2.jpg";
 
 function HomePage() {
   return (
     <Box>
-      <BannerImage />
-      <Box>home page</Box>
+      <Banner />
+      <Box m="75px 0">
+        <Image src={banner1} />
+      </Box>
+      <Content title="人工智能与编程教育" bgUrl={banner2}>
+        <Text>
+          早在2017年，国务院已出台《新一代人工智能发展规划》，要求在中小学阶段设置人工智能相关课程，逐步推广编程教育。人工智能与编程俨然成为21世纪中小学生的关键技能。
+        </Text>
+        <Text>
+          童心制物为学校开展人工智能与编程教育提供完整的软硬件服务与支持，我们将虚拟世界的编程通过现实世界中的程小奔、神经元等产品呈现出来，为学生打开好奇求知的大门，让其体验人工智能与编程的无穷魅力。
+        </Text>
+      </Content>
     </Box>
   );
 }
